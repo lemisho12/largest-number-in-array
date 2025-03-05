35d25157f3eac1ea8c1df378764770dd75612398
lemisho12 <lemitaddese94@gmail.com>
2025-03-05 15:57:13 +0300

    added a new file called adding two numbers

---
+++ adding_two_nubers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number1,number2, result;
+
+    number1=30;
+    number2=20;
+    result= number1 + number2;
+    cout<<"The sum is "<<result<<endl;
+    return 0;
+}
